6621a6ba0e65b94be75b999324d6551bffca12d2
Anisha-Chavan <anishachavan99@gmail.com>
2021-04-24 10:21:31 +0530

    hide is added to hide the question box

---
+++ js/app.js
@@ -0,0 +1,67 @@
+const questionNumber = document.querySelector(".question-number");
+const questionText =  document.querySelector(".question-text");
+const optionContainer =  document.querySelector(".option-container");
+
+let questionCounter = 0;
+let currentQuestion;
+let availableQuestions = [];
+let availableOptions = [];
+//push the question into availableQuestion Array
+function setAvailableQuestion(){
+    const totalQuestion = quiz.length;
+    for(let i=0;i<totalQuestion;i++){
+        availableQuestions.push(quiz[i])
+    }
+}
+//set question number and question and options
+function getNewQuestion(){
+    //set question number
+    questionNumber.innerHTML = "Question " + (questionCounter + 1) + " of " + quiz.length;
+    console.log(questionNumber)
+    //set question text
+    //get random question
+    const questionIndex = availableQuestions[Math.floor(Math.random() * availableQuestions.length)]
+    currentQuestion =  questionIndex;
+    questionText.innerHTML = currentQuestion.q;
+    // get the position of 'questionindex' from the availableQuestion Array
+    const index1 = availableQuestions.indexOf(questionIndex);
+    //remove the questionindex from availableQuestion Array,so that question does not repeat
+    availableQuestions.splice(index1,1);
+    //console.log(questionIndex);
+    //console.log(availableQuestions)
+    //set options
+    //get length of options
+    const optionLen = currentQuestion.options.length
+    //push option into availableOptions Array
+    for(let i=0; i<optionLen; i++){
+        availableOptions.push(i)
+
+    }
+    //ceate options in html
+    for(let i=0;i<optionLen;i++){
+        const option = document.createElement("div");
+        Option.innerHTML = currentQuestion.options[i];
+        Option.id=i;
+        Option.className = "option";
+        optionContainer.appendChild(option)
+    }
+    questionCounter++
+
+}
+function next(){
+    if(questionCounter == quiz.length){
+        console.log("quiz over");
+    }
+    else{
+        getNewQuestion();
+    }
+}
+
+
+window.onload = function(){
+    //first we will set all questions in availableQuestions Array
+    setAvailableQuestion();
+    
+    // second we will call getnewQuestion(); function
+    getNewQuestion();
+}
